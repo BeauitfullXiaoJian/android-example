bbf7071e1f4c08fb108f65257bfd27c492292da7
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2019-03-20 09:50:06 +0800

    update toolbar style

--- app/src/main/java/com/example/cool1024/android_example/CardDetailActivity.java
+++ app/src/main/java/com/example/cool1024/android_example/CardDetailActivity.java
@@ -1,15 +1,27 @@
 package com.example.cool1024.android_example;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.request.Request;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.ImageViewTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.SizeReadyCallback;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.cool1024.android_example.fragments.HomeFragment;
 
 public class CardDetailActivity extends AppCompatActivity {
@@ -18,46 +30,54 @@ public class CardDetailActivity extends AppCompatActivity {
 
     private HomeFragment.CardData cardData;
 
-    private ActionBar mActionBar;
-    private ImageView mAvatarView;
-    private ImageView mCardImageView;
-    private TextView mAuthorNickView;
-    private TextView mLabelView;
-    private TextView mDetailContent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_detail);
         loadIntentData();
-        findView();
         initView();
     }
 
-    private void findView() {
+    private void initView() {
         Toolbar toolbar = findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
-        mActionBar = getSupportActionBar();
-        mAvatarView = findViewById(R.id.author_avatar);
-        mAuthorNickView = findViewById(R.id.author_nick);
-        mLabelView = findViewById(R.id.label_string);
-        mDetailContent = findViewById(R.id.detail_content);
-        mCardImageView = findViewById(R.id.card_image);
-    }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(cardData.getCardTitle());
+            // actionBar.setSubtitle(cardData.getCardBody());
+        }
 
-    private void initView() {
-        mActionBar.setDisplayHomeAsUpEnabled(true);
-        mActionBar.setTitle(cardData.getCardTitle());
-        GlideApp.with(getBaseContext())
-                .load(cardData.getCardAvatarUrl())
-                .apply(new RequestOptions().circleCrop())
-                .into(mAvatarView);
-        GlideApp.with(getBaseContext())
-                .load(cardData.getCardImageUrl())
-                .into(mCardImageView);
-        mAuthorNickView.setText(cardData.getCardTitle());
-        mDetailContent.setText(cardData.getCardContent());
-        mLabelView.setText(cardData.getCardBody());
+        WebView webView = findViewById(R.id.web_view);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadData("<style>img{width:100%;}</style>" +
+                        "<h2>Sample</h2>" +
+                        "<p>This is an instance of the <a href=\"https://ckeditor.com/docs/ckeditor5/latest/builds/guides/overview.html#classic-editor\">classic editor build</a>.</p>" +
+                        "<img src=\"" + cardData.getCardImageUrl() + "\" />" +
+                        "<p>" + cardData.getCardContent() + "</p>"
+                , "text/html", "UTF-8");
+
+//        Target target = new SimpleTarget<Drawable>() {
+//            @Override
+//            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+//                mActionBar.setLogo(resource);
+//            }
+//        };
+//        ImageView avatarImageView = findViewById(R.id.avatar_view);
+//        GlideApp.with(this)
+//                .load(cardData.getCardAvatarUrl())
+//                .apply(new RequestOptions().circleCrop())
+//                .into(avatarImageView);
+//        GlideApp.with(getBaseContext())
+//                .load(cardData.getCardAvatarUrl())
+//                .apply(new RequestOptions().circleCrop())
+//                .into(mAvatarView);
+//        GlideApp.with(getBaseContext())
+//                .load(cardData.getCardImageUrl())
+//                .into(mCardImageView);
+//        mAuthorNickView.setText(cardData.getCardTitle());
+//        mDetailContent.setText(cardData.getCardContent());
+//        mLabelView.setText(cardData.getCardBody());
     }
 
     @Override
